0855667fc9bf933c2b817432d06abb188e8b724a
JayBassCodes <jacob.m.bassett@gmail.com>
2023-10-02 15:59:32 -0400

    wrote the SQL for my db and seed

---
+++ db/schema.sql
@@ -0,0 +1,37 @@
+DROP DATABASE IF IT EXISTS employee_db;
+CREATE DATABASE employee_db;
+USE employee_db;
+
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(10,2) NOT NULL,
+    department_id INT NOT NULL,
+    INDEX dep_id (department_id),
+    PRIMARY KEY (id),
+    FOREIGN KEY (department_id) REFERENCES department(id),
+    CONSTRAINT fk_manager
+        FOREIGN KEY (department_id)
+        REFERENCES department(id)
+        ON DELETE CASCADE
+);
+
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT NULL,
+    INDEX role_id (role_id),
+    INDEX manager_id (manager_id),
+    PRIMARY KEY (id),
+    FOREIGN KEY (role_id) REFERENCES role(id),
+    FOREIGN KEY (manager_id) REFERENCES employee(id)
+    ON DELETE SET NULL
+);
